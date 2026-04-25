a9e9a72e301b6275686f35bb67fb7eadb7e49675
GOWTHAMI-2429 <gurralagowthamig@gmail.com>
2026-04-25 12:36:56 +0530

    Add files via upload

---
+++ increment.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ void main()
+ {
+     int a;
+     printf("enter the number\n");
+     scanf("%d",&a);
+     int i= 1;
+     for(i=1;i<=5;i++)
+     {
+         printf("hii");
+     }
+     printf("i=%d",i);
+ }
+    
